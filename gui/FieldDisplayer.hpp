477ec741a6dcf1974983fca78ef737e6b6364215
nicrausaz <n.crausaz99@gmail.com>
2022-05-12 16:23:03 +0200

    saddsa

---
+++ gui/FieldDisplayer.hpp
@@ -0,0 +1,14 @@
+#ifndef POA_LABO4_FIELDDISPLAYER_HPP
+#define POA_LABO4_FIELDDISPLAYER_HPP
+
+
+#include "Field.hpp"
+
+class FieldDisplayer {
+
+    explicit FieldDisplayer(const Field& field);
+
+};
+
+
+#endif //POA_LABO4_FIELDDISPLAYER_HPP
