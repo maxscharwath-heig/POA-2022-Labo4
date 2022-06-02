3a29dd109f98947f98b99758a98a08443f2b896a
Maxime Scharwath <maxscharwath@gmail.com>
2022-06-02 13:44:09 +0200

    add input

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include "Field.hpp"
 #include "gui/FieldDisplayer.hpp"
 
@@ -10,6 +11,30 @@ int main() {
     //create a field
     Field field(50, 50, 10, 10);
     FieldDisplayer fieldDisplayer(field);
-    fieldDisplayer.display();
+
+    //todo put this in a function
+    bool continueLoop = true;
+    do {
+        //todo clear screen
+        fieldDisplayer.display();
+        std::cout << "[0] q>uit s>tatistics n>ext : ";
+        char c;
+        std::cin >> c;
+        if (!std::cin.fail()) {
+            switch (c) {
+                case 'q': //quit
+                    continueLoop = false;
+                    break;
+                case 's': //statistics
+                    break;
+                case 'n': //next
+                    break;
+                default: //do nothing
+                    break;
+            }
+        }
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } while (continueLoop);
     return EXIT_SUCCESS;
 }
